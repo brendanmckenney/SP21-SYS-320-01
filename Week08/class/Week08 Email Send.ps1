c682e826f2f23fc5f4439fc03afcfaea6210591b
brendanmckenney <43732437+brendanmckenney@users.noreply.github.com>
2021-03-20 19:32:30 -0400

    Add files via upload

---
+++ Week08/class/Week08 Email Send.ps1
@@ -0,0 +1,18 @@
+﻿# Storyline: Send an email.
+
+# Body of the email
+# Variable can have an underscore or any alphanumeric value.
+
+$msg = "Hello there, Professor Dunston."
+
+# echoing to the screen.
+write-host -BackgroundColor Red -ForegroundColor white $msg
+
+# Email From Address
+$email = "brendan.mckenney@mymail.champlain.edu"
+
+# To address
+$toEmail = "deployer@csi-web"
+
+# Sending the email
+Send-MailMessage -From $email -to $toEmail -Subject "A Greeting (Brendan McKenney)" -Body $msg -SmtpServer 192.168.6.71
