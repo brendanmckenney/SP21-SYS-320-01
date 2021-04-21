907c3fa226fa79f8df7ad3e255acb8b0696c2328
brendanmckenney <43732437+brendanmckenney@users.noreply.github.com>
2021-04-21 14:50:03 -0400

    Week 10 - Get Service Logs UPDATED

---
+++ Week10/homework/Week10_GetServiceLogs-UPDATED.ps1
@@ -0,0 +1,44 @@
+﻿function main {
+    Write-Host "Week 10 - Get Service Logs Updated Assignment"
+    Write-Host "Brendan McKenney"
+
+    $choice = Read-Host -Prompt "Please type 'all', 'running', or 'stopped', or 'Q' to quit"
+
+
+    if ($choice -match "all") {
+            $allServices = Get-Service | Out-GridView
+            Write-Host $allServices
+            sleep 2
+            main
+    }
+    if ($choice -match "running") {
+            $runningServices = Get-Service | Where Status -EQ "Running" | Out-GridView
+            Write-Host $runningServices
+            sleep 2
+            main
+    
+    }
+    if ($choice -match "stopped") {
+            $stoppedServices = Get-Service | Where Status -EQ "Stopped" | Out-GridView
+            Write-Host $stoppedServices
+            sleep 2 
+            main 
+    
+    
+    }
+
+    if ($choice -match "^[qQ]$") {
+            Write-Host "Thanks for using."
+            break
+
+    } else {
+            Write-Host -BackgroundColor Red -ForegroundColor White "Unexpected value. Returning to main menu..."
+            sleep 1
+            main
+    
+    }
+
+
+
+}
+main
