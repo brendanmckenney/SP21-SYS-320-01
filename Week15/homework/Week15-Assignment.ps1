3a0010ff7370a50c54c94f5edcab480996b918ec
brendanmckenney <43732437+brendanmckenney@users.noreply.github.com>
2021-05-06 16:21:33 -0400

    Week 15 - Completed Parsing Threat Intel Assignment

---
+++ Week15/homework/Week15-Assignment.ps1
@@ -0,0 +1,55 @@
+﻿# Storyline: Create a CSV file from the downloaded JSON file and only extract the: name, provider, url, and source_format data
+        
+$u = "https://raw.githubusercontent.com/MISP/MISP/2.4/app/files/feed-metadata/defaults.json"
+
+# Download the file
+Write-Host -BackgroundColor Red -ForegroundColor White "Downloading the required json file now... `n"
+sleep 2
+Write-Host -BackgroundColor Red -ForegroundColor White "File downloaded"
+sleep 2
+Invoke-WebRequest -Uri $u -OutFile "C:\Users\brend\Desktop\defaults.json"
+Write-Host -BackgroundColor Red -ForegroundColor White "Parsing and saving files to the desktop...."
+Write-Host -BackgroundColor Red -ForegroundColor White "DONE"
+        
+# Convert JSON file into Powershell object
+$get_defaults = (Get-Content -Raw -Path "C:\Users\brend\Desktop\defaults.json" | `
+ConvertFrom-Json) | select Feed
+
+# CSV file 
+$defaultsCSV = "C:\Users\brend\Desktop\defaults.csv"
+
+# Headers for the CSV File
+Set-Content -Value "`"name`",`"provider`",`"url`",`"source_format`"" $defaultsCSV
+
+
+#Array to store the data
+$theData = @()
+
+
+# For-each loop that loops through every instance of "Feed" and pulls the below sections (name, provider, url, source_format)
+foreach ($threat in $get_defaults.Feed) {
+            
+    #Org/domain Name 
+    $name = $threat.name
+
+    #Provider 
+    $provider = $threat.provider
+
+    # URL Data
+    $url = $threat.url
+
+    # Source_format data
+    $source_format = $threat.source_format
+
+    # format the CSV file/add the extracted content to the CSV file data array
+    $theData += "`"$name`",`"$provider`",`"$url`",`"$source_format`"`n"
+
+        
+}
+# Convert the array to a string and append to the CSV file
+"$theData" | Add-Content -Path $defaultsCSV
+
+
+
+
+
